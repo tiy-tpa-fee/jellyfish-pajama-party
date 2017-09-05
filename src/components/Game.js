d3796af8dd9068e4e8deda70f7e328f7c5f7a63a
Crystal <crystal.r.mcgowan@gmail.com>
2017-09-05 12:02:20 -0400

    Mapped through cells with game in state

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,51 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 class Game extends Component {
   state = {
     id: 240,
     board: [
-      ['_', '_', '_', '_', '_', 1, ' ', '*'],
-      ['_', '_', '_', '_', 1, 2, '*', ' '],
-      ['_', '_', '_', '_', 1, '*', ' ', ' '],
-      ['_', '_', '_', '_', 1, 1, ' ', ' '],
-      [1, 1, 1, '_', '_', 1, ' ', ' '],
-      [' ', '*', 1, '_', '_', 2, '*', '*'],
-      [' ', ' ', 1, 1, 1, 3, '*', '*'],
-      ['*', ' ', ' ', ' ', '*', ' ', ' ', ' ']
+      ["_", "_", "_", "_", "_", 1, " ", "*"],
+      ["_", "_", "_", "_", 1, 2, "*", " "],
+      ["_", "_", "_", "_", 1, "*", " ", " "],
+      ["_", "_", "_", "_", 1, 1, " ", " "],
+      [1, 1, 1, "_", "_", 1, " ", " "],
+      [" ", "*", 1, "_", "_", 2, "*", "*"],
+      [" ", " ", 1, 1, 1, 3, "*", "*"],
+      ["*", " ", " ", " ", "*", " ", " ", " "]
     ],
-    state: 'lost',
+    state: "lost",
     mines: 10,
     difficulty: 0
   }
   render() {
+    const rows = this.state.board.map(row => {
+      return (
+        <tr>
+          {row.map(col => {
+            switch (col) {
+              case " ":
+                return (
+                  <td className="unrevealed">
+                    {col}
+                  </td>
+                )
+              case "_":
+                return (
+                  <td className="revealed">
+                    {col}
+                  </td>
+                )
+              case "*":
+                return (
+                  <td className="bomb">
+                    {col}
+                  </td>
+                )
+              case "F":
+                return (
+                  <td className="flagged">
+                    {col}
+                  </td>
+                )
+              case "@":
+                return (
+                  <td className="flaggedBomb">
+                    {col}
+                  </td>
+                )
+              default:
+                return (
+                  <td className={`number-${col}`}>
+                    {col}
+                  </td>
+                )
+            }
+          })}
+        </tr>
+      )
+    })
     return (
       <div className="Game">
         <table>
           <tbody>
-            <tr>
-              <td className="unrevealed"> </td>
-              <td className="unrevealed"> </td>
-              <td className="revealed number-8">8</td>
-              <td className="flaggedBomb">@</td>
-            </tr>
-            <tr>
-              <td className="revealed">_</td>
-              <td className="revealed">_</td>
-              <td className="revealed number-1">1</td>
-              <td className="revealed number-2">2</td>
-            </tr>
-            <tr>
-              <td className="unrevealed"> </td>
-              <td className="revealed number-3">3</td>
-              <td className="bomb">*</td>
-              <td className="revealed number-4">4</td>
-            </tr>
-            <tr>
-              <td className="flagged">F</td>
-              <td className="revealed number-5">5</td>
-              <td className="revealed number-6">6</td>
-              <td className="revealed number-7">7</td>
-            </tr>
+            {rows}
           </tbody>
         </table>
       </div>
